b5d7841ac5276c3423656cfa49f8a10e426efc78
phambaongoc2221 <ngocbaopham1606@gmail.com>
2022-03-26 03:12:33 +0700

    LinearLayout

---
+++ app/src/main/java/com/example/ooclock/CreateAlarm.java
@@ -0,0 +1,14 @@
+package com.example.ooclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_alarm);
+    }
+}
