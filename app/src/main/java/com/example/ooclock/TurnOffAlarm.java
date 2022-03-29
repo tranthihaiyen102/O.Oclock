bd4863d9e07603ee841aa2314635088bd9127df3
phambaongoc2221 <ngocbaopham1606@gmail.com>
2022-03-29 17:33:12 +0700

    Giao diện tắt báo thức - Giải toán

--- app/src/main/java/com/example/ooclock/TurnOffAlarm.java
+++ app/src/main/java/com/example/ooclock/TurnOffAlarm.java
@@ -2,7 +2,9 @@ package com.example.ooclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TurnOffAlarm extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TurnOffAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turn_off_alarm);
+    }
+
+    public void turnOffAlarm(View view) {
+        Intent intent = new Intent(this, TurnOffAlarmMath.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/ooclock/TurnOffAlarmMath.java
@@ -0,0 +1,20 @@
+package com.example.ooclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
+
+public class TurnOffAlarmMath extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn_off_alarm_math);
+    }
+
+
+}
