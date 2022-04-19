33d2d175c6c3f1ebede30cee3f298cafa93dc891
Laptop TTC <linhpham270401@gmail.com>
2022-04-20 00:54:05 +0700

    add options

---
+++ app/src/main/java/com/example/ooclock/WayTurnOffActivity.java
@@ -0,0 +1,14 @@
+package com.example.ooclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WayTurnOffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_way_turn_off);
+    }
+}
