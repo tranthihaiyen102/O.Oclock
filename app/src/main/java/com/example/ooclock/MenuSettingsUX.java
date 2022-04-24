592c2468054841ab82651bfdeaba17f2d0343ad9
Laptop TTC <linhpham270401@gmail.com>
2022-04-24 17:00:07 +0700

    add new ui for Sound and Other

--- app/src/main/java/com/example/ooclock/MenuSettingsUX.java
+++ app/src/main/java/com/example/ooclock/MenuSettingsUX.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class MenuSettingsUX extends AppCompatActivity {
 
+    RadioButton radioDarkTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_settings_ux);
+
+        radioDarkTheme = findViewById(R.id.darktheme);
+//        radioDarkTheme.setChecked(true);
     }
 
     public void back(View view) {
